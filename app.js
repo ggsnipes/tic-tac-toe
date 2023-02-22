23281c8c78968dab86ea1aba4f326f592d8f1288
ggsnipes <31529149+ggsnipes@users.noreply.github.com>
2023-02-22 19:11:52 +1100

    main game working, circle design not showing up though

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ console.log(`this is not good`)
 
 //setting all the variables that need to be defined for pressing buttons
 const gameField = document.querySelectorAll('.game')
-const para = document.querySelector('p')
-const player1 = 'cross'
-const player2 = 'circle'
+const paraWinner = document.querySelector('.winner')
+const paraTurn = document.querySelector('.turn')
+const player1Class = 'x'
+const player2Class = 'circle'
 const board = document.getElementsByClassName('board')
-const restartBtn = document.getElementsByClassName('restart')
+const restartBtn = document.querySelector('.restart')
 let isPlayer2Turn = false
 const winningCombinations = [
     [0,1,2],
@@ -20,34 +21,38 @@ const winningCombinations = [
 ]
 
 
+
+
+
 //start of the game function, can also be used for resetting the game
 
 function gameStart () {
     isPlayer2Turn = false
     gameField.forEach(box => {
-        box.classList.remove(player1)
-        box.classList.remove(player2)
+        box.classList.remove(player1Class)
+        box.classList.remove(player2Class)
         box.removeEventListener('click', handleGameClick)
         box.addEventListener('click', handleGameClick, {once: true})
     })
-    setBoardHoverClass()
-    para.textContent = ''
+    //setBoardHoverClass()
+    paraWinner.textContent = ''
+    paraTurn.textContent = "It is X's turn"
 }
-
 
 //handling the clicks and turns
 
 function handleGameClick(event) {
     let div = event.target
-    const currentClass = isPlayer2Turn ? player2 : player1
+    const currentClass = isPlayer2Turn ? player2Class : player1Class
     placeMark(div, currentClass)
     if (checkWin(currentClass)) {
         endGame(false)
     } else if (isDraw()) {
         endGame(true)
     } else {
+        paraTurn.textContent = `It is ${isPlayer2Turn ? "X's" : "O's"} turn`
         swapTurns()
-        setBoardHoverClass()
+        //setBoardHoverClass()
     }
 }
 
@@ -57,9 +62,11 @@ function handleGameClick(event) {
 
 function endGame(draw) {
     if (draw) {
-        para.textContent = "It's a draw"
+        paraWinner.textContent = "It's a draw"
+        paraTurn.textContent = `Game Over`
     } else {
-        para.textContent = `Player with ${isPlayer2Turn ? "O's" : "X's"} wins`
+        paraTurn.textContent = `Game Over`
+        paraWinner.textContent = `Player with ${isPlayer2Turn ? "O's" : "X's"} wins`
     }
 }
 
@@ -67,15 +74,30 @@ function endGame(draw) {
 
 function isDraw() {
     return [...gameField].every(box => {
-        return box.classList.contains(player1) || clearInterval.classList.contains(player2)
+        return box.classList.contains(player1Class) || box.classList.contains(player2Class)
     })
 }
+
 
 //placing the icons down when clicked
 
 function placeMark (div, currentClass) {
     div.classList.add(currentClass)
 }
+
+
+
+//give a hovering effect to the tiles
+function setBoardHoverClass () {
+    board.classList.remove(player1Class)
+    board.classList.remove(player2Class)
+    if (isPlayer2Turn) {
+        board.classList.add(player2Class)
+    } else {
+        board.classList.add(player1Class)
+    }
+}
+
 
 //swap turns
 
@@ -84,16 +106,7 @@ function swapTurns() {
 }
 
 
-//give a hovering effect to the tiles
-function setBoardHoverClass () {
-    board.classList.remove(player1)
-    board.classList.remove(player2)
-    if (isPlayer2Turn) {
-        board.classList.add(player2)
-    } else {
-        board.classList.add(player1)
-    }
-}
+
 
 //check win conditions
 function checkWin(currentClass) {
@@ -120,11 +133,11 @@ function checkWin(currentClass) {
 //     }
 
 //     if (document.querySelectorAll('.click').length == 3) {
-//         para.textContent = `You are a winner!!`
+//         paraWinner.textContent = `You are a winner!!`
 //         gameField.forEach (div => 
 //             div.classList.add('winner'))
 //     } else {
-//         para.textContent = ''
+//         paraWinner.textContent = ''
 //         gameField.forEach (div => 
 //             div.classList.remove('winner'))
 //     }
@@ -133,10 +146,19 @@ function checkWin(currentClass) {
 // }
 
 
+//starts the game
+gameStart()
+
+
+
+
+//restarts the game
+restartBtn.addEventListener('click', gameStart)
+
 
 
 // restartBtn.addEventListener('click', )
 
-gameField.forEach(btn => {
-    btn.addEventListener('click', playGame)
-})
+// gameField.forEach(btn => {
+//     btn.addEventListener('click', playGame)
+// })
