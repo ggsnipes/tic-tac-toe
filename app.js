ebf25d62bf8fe8b00c73e793c8bb0a796bf033ce
ggsnipes <31529149+ggsnipes@users.noreply.github.com>
2023-02-22 11:41:46 +1100

    added clickable boxes with functions

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 console.log(`this is not good`)
 
 const gameField = document.querySelectorAll('.game')
+const para = document.querySelector('p')
 
+
+function playGame (event) {
+    let div = event.target
+
+    if (div.classList.contains('click')) {
+        div.classList.remove('click')
+    } else {
+        div.classList.add('click')
+    }
+
+    if (document.querySelectorAll('.click').length == 3) {
+        para.textContent = `You are a winner!!`
+        gameField.forEach (div => 
+            div.classList.add('winner'))
+    } else {
+        gameField.forEach (div => 
+            div.classList.remove('winner'))
+    }
+
+
+}
+
+
+
+
+gameField.forEach(btn => {
+    btn.addEventListener('click', playGame)
+})
